f51289ad5b9179def2d9d3162b87b34ded48dc60
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-02 11:42:18 +0100

    Created visuals for Hangman Gmae

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let word = prompt(`player1 picks a word`);
-alert(`player 2 guess a letter`);
-let wordArray = word.split(``);
-let lineArray = wordArray.fill(` _ `);
-let life = 10;
-while (true) {
-    let guess = prompt(`lifes: ${life} ${lineArray}`);
-    if (word.includes(guess)) {
-        for (let i = 0; i < word.length; i++) {
-            if (guess === word[i]) {
-                lineArray[i] = guess;
-            }
-        }
-    } else if (word.includes(guess) === false) {
-        life -= 1;
-    }
-    if (life === 0) {
-        alert(`you're hanged man!!`);
-        break;
-    } else if (lineArray.includes(` _ `) === false) {
-        alert(`horrraayyy!!!`);
-        break;
-    }
-}
+// let word = prompt(`player1 picks a word`);
+// alert(`player 2 guess a letter`);
+// let wordArray = word.split(``);
+// let lineArray = wordArray.fill(` _ `);
+// let life = 10;
+// while (true) {
+//     let guess = prompt(`lifes: ${life} ${lineArray}`);
+//     if (word.includes(guess)) {
+//         for (let i = 0; i < word.length; i++) {
+//             if (guess === word[i]) {
+//                 lineArray[i] = guess;
+//             }
+//         }
+//     } else if (word.includes(guess) === false) {
+//         life -= 1;
+//     }
+//     if (life === 0) {
+//         alert(`you're hanged man!!`);
+//         break;
+//     } else if (lineArray.includes(` _ `) === false) {
+//         alert(`horrraayyy!!!`);
+//         break;
+//     }
+// }
