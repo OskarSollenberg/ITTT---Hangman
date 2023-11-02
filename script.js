e07cb748252d376ac1fd09776d4b0a033653155b
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-02 18:28:51 +0100

    Woreked with js and input

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+let input = document.getElementById("userInput");
+// let word = document.getElementById("word");
+
+animate = (className, animation) => {
+    className.classList.add(animation);
+};
+resetAnimation = (element, animation) => {
+    element.classList.remove(animation);
+};
+resetValue = (element) => {
+    element.value = "";
+};
+
+let playerGuess = "";
+input.addEventListener("keypress", function (press) {
+    if (press.key === "Enter" && playerGuess.includes(input.value) === false) {
+        playerGuess += input.value;
+        console.log(playerGuess);
+
+        resetValue(input);
+    } else if (
+        press.key === "Enter" &&
+        playerGuess.includes(input.value) === true
+    ) {
+        input.classList.add("wobble");
+        // resetAnimation(input, "wobble");
+        setTimeout(resetAnimation, 1000, input, "wobble");
+
+        resetValue(input);
+    }
+});
+
 // let word = prompt(`player1 picks a word`);
-// alert(`player 2 guess a letter`);
+
 // let wordArray = word.split(``);
 // let lineArray = wordArray.fill(` _ `);
-// let life = 10;
+// let life = 5;
+
 // while (true) {
-//     let guess = prompt(`lifes: ${life} ${lineArray}`);
+//     // let guess = prompt(`lifes: ${life} ${lineArray}`);
 //     if (word.includes(guess)) {
 //         for (let i = 0; i < word.length; i++) {
 //             if (guess === word[i]) {
@@ -15,10 +48,10 @@
 //         life -= 1;
 //     }
 //     if (life === 0) {
-//         alert(`you're hanged man!!`);
+//         // alert(`you're hanged man!!`);
 //         break;
 //     } else if (lineArray.includes(` _ `) === false) {
-//         alert(`horrraayyy!!!`);
+//         // alert(`horrraayyy!!!`);
 //         break;
 //     }
 // }
